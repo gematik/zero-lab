7bc585b20f14f49d45e8e4236cac6fdc04f30afc
Sergej Suskov <108782+spilikin@users.noreply.github.com>
2024-04-07 09:33:50 +0200

    wip

---
+++ cmd/zero/tcl.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"reflect"
+
+	"github.com/gematik/zero-lab/pkg/attestation/tpmattest"
+	"github.com/gematik/zero-lab/pkg/util"
+	"github.com/google/go-attestation/attest"
+)
+
+type TrustClient struct {
+	regBaseURL string
+	tpm        *attest.TPM
+	ak         *attest.AK
+	ek         *attest.EK
+}
+
+func CreateClient(regBaseURL string, akPath string) (*TrustClient, error) {
+	config := &attest.OpenConfig{}
+	tpm, err := attest.OpenTPM(config)
+	if err != nil {
+		return nil, err
+	}
+
+	eks, err := tpm.EKCertificates()
+	if err != nil {
+		return nil, fmt.Errorf("reading EKs from TPM: %w", err)
+	}
+
+	slog.Info("TPM EKs", "count", len(eks))
+	for i, ek := range eks {
+		slog.Info("EK", "index", i, "cert", util.CertificateToText(ek.Certificate))
+	}
+
+	ek := eks[0]
+
+	ak, err := loadAK(tpm, akPath)
+	if err != nil {
+		slog.Info(fmt.Sprintf("%s. Will create new AK.", err))
+		akConfig := &attest.AKConfig{
+			Parent: &attest.ParentKeyConfig{
+				Algorithm: attest.ECDSA,
+				Handle:    0x81000002,
+			},
+		}
+		ak, err = tpm.NewAK(akConfig)
+		if err != nil {
+			return nil, err
+		}
+		err = saveAK(akPath, ak)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		slog.Info("Loaded existing AK", "path", akPath)
+	}
+
+	publicKey, err := attest.ParseAKPublic(tpm.Version(), ak.AttestationParameters().Public)
+	if err != nil {
+		return nil, fmt.Errorf("parsing AK public key: %w", err)
+	}
+
+	slog.Info("AK public key", "type", reflect.TypeOf(publicKey.Public))
+
+	return &TrustClient{
+		regBaseURL: regBaseURL,
+		tpm:        tpm,
+		ak:         ak,
+		ek:         &ek,
+	}, nil
+}
+
+func (c *TrustClient) AttestWithServer() error {
+	slog.Info("Activating TPM")
+	attestParams := c.ak.AttestationParameters()
+
+	attestationRequest, err := tpmattest.CreateAttestationRequest(c.tpm, *c.ek, &attestParams)
+	if err != nil {
+		return fmt.Errorf("creating attestation request: %w", err)
+	}
+
+	httpClient := &http.Client{}
+
+	body, err := json.Marshal(attestationRequest)
+	if err != nil {
+		return fmt.Errorf("marshaling activation request: %w", err)
+	}
+
+	slog.Info("Activation request", "body", string(body))
+	url := fmt.Sprintf("%s/tpm/attestations", c.regBaseURL)
+	resp, err := httpClient.Post(url, "application/json", bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("sending activation request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("reading response body: %w", err)
+	}
+
+	slog.Info("Activation response", "status", resp.Status, "body", string(body))
+
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	challenge := new(tpmattest.AttestationChallenge)
+	err = json.Unmarshal(body, challenge)
+	if err != nil {
+		return fmt.Errorf("unmarshaling attestation challenge: %w", err)
+	}
+
+	slog.Info("Received attestation challenge", "challenge", challenge)
+
+	secret, err := c.ak.ActivateCredentialWithEK(c.tpm, challenge.EncryptedCredential(), *c.ek)
+	if err != nil {
+		return fmt.Errorf("activating credential: %w", err)
+	}
+
+	challengeResponse := &tpmattest.AttestationChallengeResponse{
+		DecryptedSecret: secret,
+	}
+
+	body, err = json.Marshal(challengeResponse)
+	if err != nil {
+		return fmt.Errorf("marshaling challenge response: %w", err)
+	}
+
+	slog.Info("Challenge response", "body", string(body))
+
+	url = fmt.Sprintf("%s/tpm/attestations/%s", c.regBaseURL, challenge.ID)
+	resp, err = httpClient.Post(url, "application/json", bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("sending challenge response: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("reading response body: %w", err)
+	}
+
+	slog.Info("Challenge response", "status", resp.Status, "body", string(body))
+
+	return nil
+}
+
+func (c *TrustClient) Close() {
+	c.tpm.Close()
+}
+
+// Creates a certificate signing request (CSR) for the AK.
+func (c *TrustClient) CreateCSR() ([]byte, error) {
+	csrTemplate := x509.CertificateRequest{
+		Subject:            pkix.Name{CommonName: "Test Certificate"},
+		SignatureAlgorithm: x509.ECDSAWithSHA256,
+	}
+	slog.Info("csrTemplate", "csrTemplate.extraExtensions", csrTemplate.ExtraExtensions)
+	// step: generate the csr request
+	csrBytes, err := x509.CreateCertificateRequest(rand.Reader, &csrTemplate, c.ak)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create csr request: %w", err)
+	}
+	return csrBytes, nil
+}
+
+func saveAK(path string, ak *attest.AK) error {
+	akBytes, err := ak.Marshal()
+	if err != nil {
+		return fmt.Errorf("marshaling AK: %w", err)
+	}
+
+	if err := os.WriteFile(path, akBytes, 0600); err != nil {
+		return fmt.Errorf("writing AK: %w", err)
+	}
+
+	return nil
+}
+
+func loadAK(tpm *attest.TPM, path string) (*attest.AK, error) {
+	akBytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("reading AK: %w", err)
+	}
+
+	ak, err := tpm.LoadAK(akBytes)
+	if err != nil {
+		return nil, fmt.Errorf("loading AK: %w", err)
+	}
+
+	return ak, nil
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/gematik/zero-lab
 
 go 1.21.5
 
+replace github.com/google/go-attestation v0.5.1 => ../go-attestation
+
 require (
 	github.com/fxamacker/cbor/v2 v2.5.0
 	github.com/go-playground/validator/v10 v10.16.0
@@ -46,7 +48,7 @@ require (
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
